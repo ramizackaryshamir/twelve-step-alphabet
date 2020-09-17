8f4077d67e1d88147a937ae5b17fc03e2649725f
ramizackaryshamir <ramizackshamir@icloud.com>
2020-09-17 18:50:21 -0400

    setup FE architecture

---
+++ client/src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { Router } from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
