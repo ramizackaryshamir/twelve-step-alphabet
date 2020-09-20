f3c97e4337c4e1bcbfcca17d8d23e89f46865e76
ramizackaryshamir <ramizackshamir@icloud.com>
2020-09-20 19:41:35 -0400

    Meetings component backgroundColor added to body

--- client/src/screens/Meetings/Meetings.jsx
+++ client/src/screens/Meetings/Meetings.jsx
@@ -6,34 +6,40 @@ import Meeting from '../../Components/Meeting/Meeting.jsx'
 import MeetingDetail from '../MeetingDetail/MeetingDetail.jsx'
 
 const useStyles = createUseStyles({
+  body: {
+    backgroundColor: "#BAD0D9"
+  },
+  
   meetingCard: {
     display: "flex",
     flexDirection: "column",
     margin: "0px 100px 0px 100px"
-  },
+  }
 })
-
+ 
 export default function Meetings(props) {
   const { allMeetings, filterFn } = props
 
   const classes = useStyles()
 
   return (
-    <table className={classes.meetingCard}>
-      <tbody>
-        {allMeetings.filter(filterFn).map((meeting) => (
-          <Meeting
-            name={meeting.name}
-            address1={meeting.address1}
-            address2={meeting.address2}
-            city={meeting.city}
-            state={meeting.state}
-            dayOfWeek={meeting.dayOfWeek}
-            timeOfDay={meeting.timeOfDay}
-            organization={meeting.category.name}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className={classes.body}>
+      <table className={classes.meetingCard}>
+        <tbody>
+          {allMeetings.filter(filterFn).map((meeting) => (
+            <Meeting
+              name={meeting.name}
+              address1={meeting.address1}
+              address2={meeting.address2}
+              city={meeting.city}
+              state={meeting.state}
+              dayOfWeek={meeting.dayOfWeek}
+              timeOfDay={meeting.timeOfDay}
+              organization={meeting.category.name}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
