1d7d6e9aa389474785278a3223469c5ad59053e9
ramizackaryshamir <ramizackshamir@icloud.com>
2020-09-25 13:10:20 -0400

    init DELETE && UPDATE functionality for reviews

--- client/src/services/meetings.js
+++ client/src/services/meetings.js
@@ -1,43 +1,23 @@
 import api from './apiConfig'
 
-//Read All
 export const getAllMeetings = async (offset) => {
   const resp = await api.get(`/meetings?offset=${offset}`);
   return resp.data;
 }
 
-export const getAllReviews = async (id) => {
-  const resp = await api.get(`/meetings/${id}/reviews`);
-  return resp.data;
-}
-
-//Read One
 export const getOneMeeting = async (id) => {
   const resp = await api.get(`/meeting-detail/${id}`);
   return resp.data;
 
 }
 
+// export const getAllReviews = async (id) => {
+//   const resp = await api.get(`/meetings/${id}/reviews`);
+//   return resp.data;
+// }
 //Create Meeting
 // export const postMeeting = async (meetingData) => {
 //   const resp = await api.post(`/meetings`, meetingData);
 //   return resp.data;
 // }
 
-//Create Review
-export const postReview = async (formData, id) => {
-  const resp = await api.post(`/meetings/${id}/reviews`, { review: formData });
-  return resp.data;
-}
-
-//Update Review
-export const putUpdatedReview = async (id, formData) => {
-  const resp = await api.put(`/meetings/${id}`, formData);
-  return resp.data;
-}
-
-//Destroy Review
-export const destroyReview = async (id) => {
-  const resp = await api.delete(`/meetings/${id}`);
-  return resp;
-}
---
+++ client/src/services/reviews.js
@@ -0,0 +1,22 @@
+import api from './apiConfig.js'
+
+ export const getAllReviews = async (id) => {
+  const resp = await api.get(`/meetings/${id}/reviews`);
+  return resp.data;
+ }
+
+//Create Review
+export const postReview = async (formData, id) => {
+  const resp = await api.post(`/meetings/${id}/reviews`, { review: formData });
+  return resp.data;
+}
+//Destroy Review
+export const destroyReview = async (id) => {
+  const resp = await api.delete(`/meetings/${id}`);
+  return resp.data;
+}
+//Update Review
+export const updateReview = async (id, formData) => {
+  const resp = await api.put(`/meetings/${id}`, formData);
+  return resp.data;
+}
