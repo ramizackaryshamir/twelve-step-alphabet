22925ff2c5f85f13ad05ff83c0724a47c8f9e61c
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-09 15:35:00 -0400

    removed Map component from MeetingsContainer as it was unnecessary

--- client/src/containers/MeetingsContainer/MeetingsContainer.jsx
+++ client/src/containers/MeetingsContainer/MeetingsContainer.jsx
@@ -4,7 +4,7 @@ import { getAllMeetings, getOneMeeting } from "../../services/meetings.js"
 
 import Meetings from "../../screens/Meetings/Meetings.jsx"
 import MeetingDetail from "../../screens/MeetingDetail/MeetingDetail.jsx"
-import Map from "../../Components/Map/Map.jsx"
+// import Map from "../../Components/Map/Map.jsx"
 
 const MeetingsContainer = () => {
   
@@ -70,9 +70,6 @@ const MeetingsContainer = () => {
         <Route path='/meeting-detail/:id'>
           <MeetingDetail
             allMeetings={allMeetings}
-            oneMeeting={oneMeeting}
-          />
-          <Map
             oneMeeting={oneMeeting}
           />
         </Route>
