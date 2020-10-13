d936fdb650631a02f1f5cf5bbd9100e10368e30a
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-13 16:41:03 -0400

    marker renders to coreect coordinates on map, initial zoom set to 12

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import useStyles from './Map.js'
-import Marker from '../Marker.jsx'
+import { Marker } from '../../Assets/mapbox-icon.png'
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmFtaXphY2thcnlzaGFtaXIiLCJhIjoiY2tjajRzMXA5MWMyczJybnFoMzB0cGFveiJ9.dJGkd1gcu3cPQ_l46OQT7w";
@@ -13,12 +13,6 @@ const Map = ({ longitude, latitude }) => {
     longitude: longitude.toFixed(5)
     
   })
-  
-  const [marker, setMarker] = useState({
-    latitude: latitude.toFixed(5),
-    longitude: longitude.toFixed(5),
-  })
-  
 
   const classes = useStyles()
   //useRefs to store references to the map  object and the map html element
@@ -32,27 +26,30 @@ const Map = ({ longitude, latitude }) => {
       style: "mapbox://styles/mapbox/streets-v11",
       // This is where the coordinates render to Map, e.g.:
       // center: [40.60942, -74.27337]
-      center: [longitude, latitude]
+      center: [longitude, latitude],
+      zoom: 12
     })
-    setMarker(marker);
-    setViewport(viewport);
 
     //Get location of user
-    map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true
-    }));
-    
-    const renderData = (marker) => {
-        // create a HTML element for each feature
-        //make a marker for each feature and add to the map
-        new mapboxgl.Marker(marker).setLngLat([longitude, latitude]).addTo(map)
-      }
-    
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
+    )
 
+    //create a marker for meeting coordinates and render marker to map
+    const renderData = (marker) => {
+      new mapboxgl.Marker(marker).setLngLat([longitude, latitude]).addTo(map)
+    }
     renderData()
+
+    setViewport(map.getCenter().lng.toFixed(4));
+    setViewport(map.getCenter().lat.toFixed(4));
+
+    //   setState(map.getCenter().lat.toFixed(4));
     //Add navigation control
     // map.addControl(new mapboxgl.NavigationControl(), "bottom-left")
     // map.on('move', () => {
@@ -62,18 +59,12 @@ const Map = ({ longitude, latitude }) => {
     // });
 
     //Clean up on unmount
-    return () => map.remove();
+    return () => map.remove()
   }, []);
 
   return (
     <div>
       <div className={classes.mapContainer} ref={mapContainerRef}>
-        <Marker
-          setMarker={marker}
-          setViewport={viewport}
-          longitude={longitude}
-          latitude={latitude}
-        />
         <div className={classes.sidebar}>
           <div>
             Longitude: {longitude} | Latitude: {latitude} | Zoom:{" "}
