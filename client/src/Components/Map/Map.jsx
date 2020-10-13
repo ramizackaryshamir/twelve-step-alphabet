6efa59195ffad02e5c5079fec716075d55d9e391
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-13 16:54:42 -0400

    cleaned up code

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import useStyles from './Map.js'
-import { Marker } from '../../Assets/mapbox-icon.png'
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmFtaXphY2thcnlzaGFtaXIiLCJhIjoiY2tjajRzMXA5MWMyczJybnFoMzB0cGFveiJ9.dJGkd1gcu3cPQ_l46OQT7w";
@@ -15,11 +14,11 @@ const Map = ({ longitude, latitude }) => {
   })
 
   const classes = useStyles()
+
   //useRefs to store references to the map  object and the map html element
   const mapContainerRef = useRef();
   
   //Initialize map when component mounts
-
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
@@ -46,31 +45,17 @@ const Map = ({ longitude, latitude }) => {
     }
     renderData()
 
-    setViewport(map.getCenter().lng.toFixed(4));
-    setViewport(map.getCenter().lat.toFixed(4));
-
-    //   setState(map.getCenter().lat.toFixed(4));
-    //Add navigation control
-    // map.addControl(new mapboxgl.NavigationControl(), "bottom-left")
-    // map.on('move', () => {
-    //   setState(map.getCenter().lng.toFixed(4));
-    //   setState(map.getCenter().lat.toFixed(4));
-    //   setState(map.getZoom().toFixed(2));
-    // });
+    setViewport(map.getCenter(viewport).lng.toFixed(4));
+    setViewport(map.getCenter(viewport).lat.toFixed(4));
 
     //Clean up on unmount
     return () => map.remove()
+
   }, []);
 
   return (
     <div>
-      <div className={classes.mapContainer} ref={mapContainerRef}>
-        <div className={classes.sidebar}>
-          <div>
-            Longitude: {longitude} | Latitude: {latitude} | Zoom:{" "}
-            {viewport.zoom}
-          </div>
-        </div>
+      <div className={classes.mapContainer} ref={mapContainerRef} setViewport={viewport}>
       </div>
     </div>
   )
