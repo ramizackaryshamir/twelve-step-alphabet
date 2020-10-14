a7e2ceeb0d6d9af062a6004b974c7b94dc24aae1
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-14 09:41:44 -0400

    popup renders with nanm, address1, address2, city, and state props

--- client/src/Components/Map/Map.jsx
+++ client/src/Components/Map/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
-import useStyles from './Map.js'
+import './Map.css'
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmFtaXphY2thcnlzaGFtaXIiLCJhIjoiY2tjajRzMXA5MWMyczJybnFoMzB0cGFveiJ9.dJGkd1gcu3cPQ_l46OQT7w";
@@ -15,8 +15,6 @@ const Map = ({ longitude, latitude, name, address1, address2, city, state }) => 
     city: city,
     state: state
   })
-
-  const classes = useStyles()
 
   //useRefs to store references to the map  object and the map html element
   const mapContainerRef = useRef()
@@ -44,11 +42,9 @@ const Map = ({ longitude, latitude, name, address1, address2, city, state }) => 
 
     //create a marker for meeting coordinates and render marker to map
     const renderData = (marker) => {
-      new mapboxgl.Marker(marker)
-        .setLngLat([longitude, latitude])
-        .setPopup(new mapboxgl.Popup({ offset: 20 }).setHTML(`<h3> ${ name } <h5> ${address1} <br> ${address2} <br> ${city}, ${state}`))
-        .addTo(map)
+      new mapboxgl.Marker(marker).setLngLat([longitude, latitude]).setPopup(new mapboxgl.Popup({ offset: 20, className: 'popup-box' }).setHTML(`<h3> ${ name } <h5> ${address1} <br> ${address2} <br> ${city}, ${state}`)).addTo(map)
     }
+   
     renderData()
 
     setViewport(map.getCenter(viewport).lng.toFixed(4))
@@ -61,7 +57,7 @@ const Map = ({ longitude, latitude, name, address1, address2, city, state }) => 
   return (
     <div>
       <div
-        className={classes.mapContainer}
+        className='map-container'
         ref={mapContainerRef}
         setViewport={viewport}
       ></div>
