ca6a2c130cedc93d198d1e3adf6b55953c2287ca
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-30 12:36:58 -0400

    color restore Meetings.jsx, line 5

--- client/src/screens/Meetings/Meetings.js
+++ client/src/screens/Meetings/Meetings.js
@@ -2,7 +2,7 @@ import {createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
   body: {
-    backgroundColor: "#BAD0D9",
+    backgroundColor: "#79e1fd",
   },
 
   meetingCard: {
