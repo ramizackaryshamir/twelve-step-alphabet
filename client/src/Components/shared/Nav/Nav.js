d78912db42706fc6e4e1f83c7c9f1f8dd1f6a0d2
ramizackaryshamir <ramizackshamir@icloud.com>
2020-12-02 13:18:59 -0500

    update @media from 415px to 505px ALL

--- client/src/Components/shared/Nav/Nav.js
+++ client/src/Components/shared/Nav/Nav.js
@@ -25,7 +25,7 @@ const useStyles = createUseStyles({
     color: "#191919",
   },
 
-  '@media (max-width: 415px)': {
+  '@media (max-width: 505px)': {
     navBar: {
       width: '100%',
       height: 35,
